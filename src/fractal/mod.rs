0749e232d5184a99dd3e02449ab75424fc084e72
lupuuss <damian.baczynski@gmail.com>
2020-02-26 15:40:44 +0100

    Moves generators implementations to separate module

--- src/fractal/mod.rs
+++ src/fractal/mod.rs
@@ -1,11 +1,15 @@
 pub mod math;
 pub mod trans;
 pub mod config;
+mod generators;
 
 use math::{ComplexRangeF64, ComplexF64, Range};
 use trans::FramePart;
 use config::FramePartConfig;
+use generators::{Mandelbrot, JuliaSet};
+
 use super::utils::worker::Worker;
+
 use std::sync::{Arc, RwLock};
 
 pub enum Fractal {
@@ -143,102 +147,5 @@ pub trait FractalGenerator {
         }
 
         FramePart::new(config.lines(), frame_part)
-    }
-}
-
-pub struct Mandelbrot {
-    pixel_size: (usize, usize),
-    max_iter: u16
-}
-
-impl Mandelbrot {
-    pub fn new(pixel_size: (usize, usize), max_iter: u16) -> Self {
-        Mandelbrot {
-            pixel_size: pixel_size,
-            max_iter: max_iter
-        }
-    }
-}
-
-impl FractalGenerator for Mandelbrot {
-
-    fn constant(&self) -> ComplexF64 {
-        ComplexF64 {
-            re: 0.0,
-            im: 0.0
-        }
-    }
-
-    fn frame_pixel_size(&self) -> (usize, usize) {
-        self.pixel_size
-    }
-
-    fn max_iterations(&self) -> u16 {
-        self.max_iter
-    }
-
-    fn convergence_iterations(&self, max_iter: u16, c: ComplexF64, constant: ComplexF64) -> u16 {
-        let mut i = 0;
-        let mut result = constant;
-
-        while result.re * result.re + result.im * result.im < 4.0 && i < max_iter {
-
-            result = ComplexF64::mul(result, result);
-            result = ComplexF64 {
-                re: result.re + c.re, im: result.im + c.im
-            };
-
-            i += 1;
-        }
-
-        return i;
-    }
-}
-
-pub struct JuliaSet {
-    pixel_size: (usize, usize),
-    max_iter: u16,
-    constant: ComplexF64
-}
-
-impl JuliaSet {
-    pub fn new(pixel_size: (usize, usize), max_iter: u16, constant: ComplexF64) -> Self {
-        JuliaSet {
-            pixel_size: pixel_size,
-            max_iter: max_iter,
-            constant: constant
-        }
-    }
-}
-
-impl FractalGenerator for JuliaSet {
-
-    fn constant(&self) -> ComplexF64 {
-        self.constant
-    }
-
-    fn frame_pixel_size(&self) -> (usize, usize) {
-        self.pixel_size
-    }
-
-    fn max_iterations(&self) -> u16 {
-        self.max_iter
-    }
-
-    fn convergence_iterations(&self, max_iter: u16, c: ComplexF64, constant: ComplexF64) -> u16 {
-        let mut i = 0;
-        let mut result = c;
-
-        while result.re * result.re + result.im * result.im < 4.0 && i < max_iter {
-
-            result = ComplexF64::mul(result, result);
-            result = ComplexF64 {
-                re: result.re + constant.re, im: result.im + constant.im
-            };
-
-            i += 1;
-        }
-
-        return i;
     }
 }
